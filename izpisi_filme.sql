112638a558cd8d417aa4caa8a511ec19af76fb62
juriko123 <jurij.zamarkodelja19@gmail.com>
2024-10-21 12:32:16 +0200

    Razni ukazi

--- izpisi_filme.sql
+++ izpisi_filme.sql
@@ -9,5 +9,19 @@
 --     FROM film
 -- );
 
+#zdruzevanje
 
+-- Povprečna ocena filmov iz leta 2000
+SELECT AVG(ocena) FROM filmi
+WHERE leto = 2000;
+
+-- Povprečna ocena filmov za vsako leto
+SELECT leto, AVG(ocena) FROM filmi
+GROUP BY leto;
+
+-- Povprečna ocena filmov za vsako leto, ko je bilo vsaj 5 filmov
+SELECT leto, AVG(ocena) AS povprecna_ocena, COUNT(*) AS st_filmov
+FROM filmi
+GROUP BY leto
+HAVING st_filmov > 5;
  
